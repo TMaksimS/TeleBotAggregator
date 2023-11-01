08fdcf8c3e8adadc7c1cace7dbe415ca42fa1f6b
TMaksimS <williamcano97@gmail.com>
2023-11-01 14:35:17 +0400

    Created Agregator logic

--- src/database/crud.py
+++ src/database/crud.py
@@ -2,10 +2,21 @@ import bson
 
 from src.database import mycoll
 
-
 async def insert_data() -> None:
     """Метод вставляет в БД данные"""
     with open("src/database/sample_collection.bson", "rb") as f:
         data = bson.decode_all(f.read())
         mycoll.insert_many(data)
     return None
+
+
+async def get_values(date_start, date_stop) -> int:
+    """Метод возвращает сумму значений за выбранный период
+    (изучить agreggator и $sum)"""
+    query = {
+        "dt": {
+            "$gte": date_start,
+            "$lt": date_stop
+        }}
+    res = [i["value"] for i in mycoll.find(query, {"_id": 0, "value": 1})]
+    return sum(res)
